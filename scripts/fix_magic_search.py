54675a3d4af2005ea1972271879c44082ed6c247
saazautomation <support@saazautomation.com>
2026-05-21 06:57:54 +0500

    old removed

--- scripts/fix_magic_search.py
+++
@@ -1,6 +0,0 @@
-path = "components/MagicAiSearchSection.tsx"
-lines = open(path, encoding="utf-8").read().splitlines()
-# Line 109 (index 108) closes magic-search-inner (a plain div).
-lines[108] = "      </div>"
-open(path, "w", encoding="utf-8").write("\n".join(lines) + "\n")
-print("fixed:", repr(lines[108]))
